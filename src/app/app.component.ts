28b24e1b111b73fbb79e0935aecc31ef87ff248b
guelsah15 <guelsah15@schule.bremen.de>
2024-01-18 19:42:43 +0100

    suchfeld wurde erstellt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,11 +8,12 @@ import {AddQualificationComponent} from "./components/add-qualification/add-qual
 import {EmployeeDetailsComponent} from "./components/employee-details/employee-details.component";
 import {Employee} from "./Employee";
 import {DataService} from "./services/data-service";
+import {SearchBarComponent} from "./search-bar/search-bar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, EmployeeListComponent, QualificationCardComponent, RestService, QualificationListComponent, AddQualificationComponent, EmployeeDetailsComponent],
+  imports: [CommonModule, EmployeeListComponent, QualificationCardComponent, RestService, QualificationListComponent, AddQualificationComponent, EmployeeDetailsComponent, SearchBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
